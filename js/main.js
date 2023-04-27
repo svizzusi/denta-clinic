03289dd7726d6044982aba077905be62c6482242
Stephen Vizzusi <svizzusi13@gmail.com>
2023-04-27 15:07:49 -0700

    Finished all HTML, CSS, and JS for home page. Need to add media queries in the future

--- js/main.js
+++ js/main.js
@@ -15,14 +15,38 @@ function scrollFunction() {
 }
 
 // swiper
-let swiper = new Swiper('.swiper-container', {
-  // Set your desired options here
-  loop: true, // enable loop mode
-  pagination: {
-    el: '.swiper-pagination', // specify the pagination element
-  },
-  navigation: {
-    nextEl: '.swiper-button-next', // specify the next button element
-    prevEl: '.swiper-button-prev', // specify the previous button element
-  },
-});
+function reviewSection() {
+  let swiper = new Swiper('.review-container .swiper-container', {
+    // Set your desired options here
+    loop: true, // enable loop mode
+    pagination: {
+      el: '.swiper-pagination', // specify the pagination element
+    },
+    navigation: false,
+    scrollbar: false,
+    slidesPerView: 1,
+    autoplay: {
+      delay: 5000, // set the time delay between slides in milliseconds
+      disableOnInteraction: false, // set to false to prevent autoplay from stopping on user interaction
+      },
+  });
+}
+reviewSection();
+
+function patientCareSection() {
+  let swiper = new Swiper('.patient-care-container .swiper-container', {
+    // Set your desired options here
+    loop: true, // enable loop mode
+    pagination: {
+      el: '.swiper-pagination', // specify the pagination element
+    },
+    navigation: false,
+    scrollbar: false,
+    slidesPerView: 3,
+    autoplay: {
+      delay: 5000, // set the time delay between slides in milliseconds
+      disableOnInteraction: false, // set to false to prevent autoplay from stopping on user interaction
+      },
+  });
+}
+patientCareSection();
